84e2fa883409e3b67a1d1583ad04f9d63e04df66
vihvch <vihvch@gmail.com>
2021-09-18 12:04:00 +0800

    引入Vant組件庫,設置移動端REM適配

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ import store from './store'
 // 加載全局樣式
 import './styles/index.less'
 
+// 加載Vant組件庫
+import Vant from 'vant'
+
+// 加載Vant組件庫樣式
+import 'vant/lib/index.css'
+
+// 自動設置 REM 基準值 (html 標籤字體大小)
+import 'amfe-flexible'
+
+// 全局註冊Vant中的組件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 // 創建Vue根實例，將router, store配置到根實例中
